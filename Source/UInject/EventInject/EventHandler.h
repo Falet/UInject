1270029792869d2d636516fc40d42e32c0be5382
Falet <miha.m1@yandex.ru>
2023-06-08 20:28:10 +0700

    Add EventHandler

---
+++ Source/UInject/EventInject/EventHandler.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+template<class NameEvent>
+class TPublicEvent final
+{
+private:
+	NameEvent* _event;
+
+public:
+	explicit TPublicEvent(NameEvent* Event)
+		: _event(Event)
+	{
+	}
+
+	FDelegateHandle Add(typename NameEvent::FDelegate& Delegate)
+	{
+		return _event->Add(Delegate);
+	}
+
+	bool Remove(FDelegateHandle Handle)
+	{
+		return _event->Remove(Handle);
+	}
+};
+
+template <class Owner, class NameEvent>
+class TEventHandler
+{
+public:
+
+	TPublicEvent<NameEvent> GetPublicEvent()
+	{
+		return _publicEvent;
+	}
+	
+private:
+	friend Owner;
+	NameEvent Event;
+	TPublicEvent<NameEvent> _publicEvent = TPublicEvent<NameEvent>(&Event);
+};
+
